91dce6a6fb0bd43e450105fb35cfab681cbb9ecb
MalinAhlberg <malin.ahlberg@nbis.se>
2021-03-05 15:00:08 +0100

    Fix sparv talk, add progress bar

--- sparv.py
+++ sparv.py
@@ -9,7 +9,7 @@ import urllib.request
 def annotate(sent):
     """Get annotations for a sentence."""
 
-    sent = re.sub('\s+', '+', urllib.parse.quote(sent))
+    sent = urllib.parse.quote(sent)
     url = f'https://ws.spraakbanken.gu.se/ws/sparv/v2/?text={sent}'
     with urllib.request.urlopen(url) as response:
         ans = response.read()
@@ -22,3 +22,32 @@ def add_xml(sentence):
     db.Sentence.update({db.Sentence.xml: xml}).where(
         db.Sentence.id == sentence.id
     ).execute()
+
+
+def annotate_selection(selection):
+    done = 0
+    print_progressbar(done, len(selection))
+    for sent in selection:
+        add_xml(sent)
+        done += 1
+        print_progressbar(done, len(selection))
+
+
+# Print iterations progress
+def print_progressbar(iteration, total, decimals=1, length=100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = '_' * filledLength + '-' * (length - filledLength)
+    print(f'\r |{bar}| {percent}% ', end="\r")
+    # Print New Line on Complete
+    if iteration == total:
+        print()
